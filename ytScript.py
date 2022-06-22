38346c53e19cbabf01916c37e708ed518c148bf6
Max Sikora <msikora8@gatech.edu>
2022-06-22 10:24:24 +0200

    Comments

--- ytScript.py
+++ ytScript.py
@@ -1,11 +1,14 @@
+#Import statements
 import json
-
 from googleapiclient.discovery import build
 
+#Api Key generated 
 api_key = 'AIzaSyBYoNWt_KjSIYSQYo55j2nXf_3_oD9N29k'
 
+#Building Youtube object
 youtube = build('youtube', 'v3', developerKey=api_key)
 
+#List of keywords
 keyword_list = []
 keyword_list.append("Computer Science Lists")
 keyword_list.append("Computer Science Stacks")
@@ -19,8 +22,10 @@ keyword_list.append("Computer Science Dynamic Programming")
 keyword_list.append("Computer Science Data Structures")
 keyword_list.append("Computer Science Algorithms")
 
+#File path with type "write"
 f = open("query_responses.txt", 'w')
 
+#Iterate through the keyword list to search youtube api and write out to query_responses.txt
 for string in keyword_list:
     request = youtube.search().list(
     part="snippet",
