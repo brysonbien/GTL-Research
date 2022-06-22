01b95324ec99a23bab31fbee08a880d625eb8004
ewillner3 <ewillner3@gatech.edu>
2022-06-22 09:28:01 +0200

    moved to video access

--- ytScript.py
+++ ytScript.py
@@ -4,13 +4,14 @@ api_key = 'AIzaSyBYoNWt_KjSIYSQYo55j2nXf_3_oD9N29k'
 
 youtube = build('youtube', 'v3', developerKey=api_key)
 
-request = youtube.channels().list(
-    part='statistics' ,
-    forUsername='PewDiePie'
+request = youtube.videos().list(
+    part="snippet",
+    q="data structures and algorithms",
+    maxResults=2
+
 )
 
 response = request.execute()
 
 print(response)
 
-#ssdfusudfudfh
