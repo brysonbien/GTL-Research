22e5dd09939ada6b3319b5c6dd01917ae4626caa
ewillner3 <ewillner3@gatech.edu>
2022-06-27 15:41:26 +0200

    removed header, videoId list

--- ytScript.py
+++ ytScript.py
@@ -33,11 +33,22 @@ for string in keyword_list:
     q = string,
     maxResults = 10
     )
-    header = "\n#################################################\n" + "\n#################################################\n"+ string + "\n#################################################\n"+ "\n#################################################\n"
 
     with open("query_responses.txt", 'a') as f:
         response = request.execute()
         #See yotube api request in terminal
             #print(response)
-        f.write(header)
         json.dump(response, f, indent=4)
+
+# Reads videoIds from response .txt document and writes to a new file
+with open('query_videoIds.txt','w') as n:
+
+with open('query_responses.txt') as f:
+    content = f.read()
+
+    d = json.load(f)
+    videoIds = []
+    for elem in d['items']:
+        videoIds.append(elem["id"]["videoId"])
+
+json.dump(videoIds, n, indent=4)
