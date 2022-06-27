559d30b36901d56a6196f7df2ad7367d9c31b8ea
ewillner3 <ewillner3@gatech.edu>
2022-06-27 16:52:23 +0200

    fixed json videoId list

--- ytScript.py
+++ ytScript.py
@@ -3,7 +3,7 @@ import json
 from googleapiclient.discovery import build
 
 #Api Key generated 
-api_key = 'AIzaSyCAhs1mzIC6QCQTxaNRkF6Fg1U7KVliLgg'
+api_key = 'AIzaSyDIuQ0gDsXrKNvGIdKMWUezUjkD9Kgrx8s'
 
 #Building Youtube object
 youtube = build('youtube', 'v3', developerKey=api_key)
@@ -11,19 +11,19 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 #List of keywords
 keyword_list = []
 keyword_list.append("Computer Science Lists")
-keyword_list.append("Computer Science Stacks")
-keyword_list.append("Computer Science Queues")
-keyword_list.append("Computer Science Linked Lists")
-keyword_list.append("Computer Science Trees, AVL & Binary Trees ")
-keyword_list.append("Computer Science Graphs")
-keyword_list.append("Computer Science Trees")
-keyword_list.append("Computer Science Hashing and hash tables")
-keyword_list.append("Computer Science Dynamic Programming")
-keyword_list.append("Computer Science Data Structures")
-keyword_list.append("Computer Science Algorithms")
+# keyword_list.append("Computer Science Stacks")
+# keyword_list.append("Computer Science Queues")
+# keyword_list.append("Computer Science Linked Lists")
+# keyword_list.append("Computer Science Trees, AVL & Binary Trees ")
+# keyword_list.append("Computer Science Graphs")
+# keyword_list.append("Computer Science Trees")
+# keyword_list.append("Computer Science Hashing and hash tables")
+# keyword_list.append("Computer Science Dynamic Programming")
+# keyword_list.append("Computer Science Data Structures")
+# keyword_list.append("Computer Science Algorithms")
 
 #File path with type "write"
-f = open("query_responses.json", 'w')
+# f = open("query_responses.json", 'w')
 
 #Iterate through the keyword list to search youtube api and write out to query_responses.txt
 for string in keyword_list:
@@ -31,21 +31,14 @@ for string in keyword_list:
     part="snippet",
     type='video',
     q = string,
-    maxResults = 1
+    maxResults = 2
     )
 
-    with open("query_responses.json", 'a') as f:
-        response = request.execute()
-        #See yotube api request in terminal
-            #print(response)
-        json.dump(response, f, indent=4)
+    response = request.execute()
 
-# Reads videoIds from response .txt document and writes to a new file
-with open('query_responses.json') as f:
-    content = f.read()
-
-    d = json.load(f)
+# Reads videoIds from response .json document and writes to a new file
+with open('query_responses.csv', 'w') as f:
     videoIds = []
-    for elem in d['items']:
+    for elem in response['items']:
         videoIds.append(elem["id"]["videoId"])
-    print(','.join(videoIds))
+    f.write(','.join(videoIds))
