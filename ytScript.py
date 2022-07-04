f48508f0b84e13ff5a56f93ade27bcaa9c8d8937
Max Sikora <msikora8@gatech.edu>
2022-07-04 19:16:21 +0200

    Outputs to Csv File with stats

--- ytScript.py
+++ ytScript.py
@@ -6,7 +6,8 @@ from googleapiclient.discovery import build
 #Api Key generated 
 api_key = 'AIzaSyDIuQ0gDsXrKNvGIdKMWUezUjkD9Kgrx8s'
 
-csv_output = 'query_responses.csv'
+csv_output = 'videoIdAndStats.csv'
+comments_output = 'comments.csv'
 
 #Building Youtube object
 youtube = build('youtube', 'v3', developerKey=api_key)
@@ -29,66 +30,88 @@ keyword_list = ["Computer Science Lists",
 Gets the videoIds of the imported search parameters, stores them in a csv file.
 It then uses those CSV to get the top 50 comments for each video.
 """
-def csvOutputVideos():
-    print("####################################\n" + "COLLECTING VIDEO IDS\n" + "####################################")
+def getVideos(topic):
     #Iterate through the keyword list to search youtube api and write out to query_responses.txt
     videoIds = {}
-    for string in keyword_list:
-        request = youtube.search().list(part="snippet",
-                                        type='video',
-                                        q = string,
-                                        maxResults = 2)
-        response = request.execute()
+    request = youtube.search().list(part="snippet",
+                                    type='video',
+                                    q = topic,
+                                    maxResults = 2)
+    response = request.execute()
 
-        nextPageToken = response["nextPageToken"]
+    nextPageToken = response["nextPageToken"]
 
-        for elem in response['items']:
-            videoId = elem["id"]["videoId"]
-            publishedAt = elem["snippet"]["publishedAt"]
-            channelId = elem["snippet"]["channelId"]
-            title = elem["snippet"]["title"] 
+    for elem in response['items']:
+        videoId = elem["id"]["videoId"]
+        publishedAt = elem["snippet"]["publishedAt"]
+        channelId = elem["snippet"]["channelId"]
+        title = elem["snippet"]["title"] 
 
-            videoObject =  [title, 
-                            publishedAt, 
-                            channelId, 
-                            nextPageToken]
+        videoObject =  [topic,
+                        title, 
+                        publishedAt, 
+                        channelId, 
+                        nextPageToken]
 
-            videoIds[videoId] = videoObject
-    
-    print("####################################\n" + "SAVING VIDEO IDS\n" + "####################################")
-
-    with open(csv_output, 'w+') as f:
-        f.write(",".join([  "topic",
-                            "title",
-                            "publishedAt",
-                            "videoId",
-                            "channelId",
-                            "nextPageToken"]))
-    with open(csv_output, 'a') as f:
-        for vidId in videoIds.keys():
-            f.write("\n" +
-                    ",".join([videoIds[vidId][0],
-                              videoIds[vidId][1],
-                              vidId,
-                              videoIds[vidId][2],
-                              videoIds[vidId][nextPageToken]]))
+        videoIds[videoId] = videoObject
+    return videoIds
                               
 """
-Gets video statistics from video ID
+Gets video statistics from video IDs
+
+"statistics": {
+    "viewCount": unsigned long,
+    "likeCount": unsigned long,
+    "dislikeCount": unsigned long,
+    "favoriteCount": unsigned long,
+    "commentCount": unsigned long
+  }
+
 """
-def getStatistics():
-    print("####################################\n" + "VIDEO_IDS USED FOR STATS\n" + "####################################")
-    
-    
+def getStatistics(videoIds):
+    videoStats = {}
+    for videoId in videoIds.keys():
+            request = youtube.videos().list(part = "id, statistics",
+                                            id = videoId
+                                            )
+            response = request.execute()
+
+            videoStats[videoId] = response["items"][0]["statistics"]
+    return videoStats
+
+"""
+Iterate through the keyword list to search youtube api and write out to query_responses.csv
+"""
+def getComments(videoIds):
+    videoComments = {}
+    for video_Id in videoIds:
+
+        request = youtube.commentThreads().list(videoId = video_Id,
+                                                part = "snippet,replies",
+                                                order = "relevance",
+                                                maxResults = 50)
+        try:
+            response = request.execute()
+            for elem in response['items']:
+               videoComments[video_Id] = elem['snippet']['topLevelComment']['snippet']['textDisplay']
+        except:
+            print(video_Id + " has comments disabled, or something else went wrong")
+    print("\n")
+
+"""
+Gets channel statistics from channel ID
+"""
+def getChannelStats():
+    print("####################################\n" + "CHANNEL_IDS USED FOR STATS\n" + "####################################")
     with open('query_responses.csv', 'r') as f:
         data = f.read()
         split_data = data.split(",")
         split_data.reverse()
         print("\n")
-        for videoId in split_data:
-            request = youtube.videos().list(
+        for channelId in split_data:
+            request = youtube.channels().list(
                 part = "id, statistics",
-                id = videoId
+                id = channelId
                 )
             response = request.execute()
             print(response["items"][0]["id"])
@@ -96,34 +119,58 @@ def getStatistics():
             print("\n")
     print("\n")
 
-"""
-Iterate through the keyword list to search youtube api and write out to query_responses.csv
-"""
-def csvOutputComments():
-    print("####################################\n" + "VIDEO_IDS USED FOR COMMENTS\n" + "####################################")
-    with open('query_responses.csv', 'r') as f:
-            data = f.read()
-            split_data = data.split(",")
-            split_data.reverse()
-            for video_Id in split_data:
-                print("\n" + video_Id)
-                request = youtube.commentThreads().list(
-                    videoId = video_Id,
-                    part = "snippet,replies",
-                    order = "relevance",
-                    maxResults = 50
-                    )
-                try:
-                    response = request.execute()
-                    for elem in response['items']:
-                        print("\n" + elem['snippet']['topLevelComment']['snippet']['textDisplay'])
-                    print('\n')
-                except:
-                    print(video_Id + " has comments disabled, or something else went wrong")
-    print("\n")
-
 def main():
-        csvOutputVideos()
-        # csvOutputComments()
-        # getStatistics()
+        #Gather Video Stats
+        with open(csv_output, 'w+') as f:
+            f.write(",".join([  "topic",
+                                "title",
+                                "publishedAt",
+                                "videoId",
+                                "channelId",
+                                "viewCount",
+                                "likeCount",
+                                "favoriteCount",
+                                "commentCount",
+                                "nextPageToken"]))
+        
+        with open(comments_output, 'w+') as f:
+            f.write(",".join([  "topic",
+                                "title",
+                                "publishedAt",
+                                "videoId",
+                                "channelId",
+                                "viewCount",
+                                "likeCount",
+                                "favoriteCount",
+                                "commentCount",
+                                "nextPageToken"]))
+        
+
+        for topic in keyword_list:
+            print("\n####################################\n" + "COLLECTING VIDEO IDS FOR " + topic + "\n####################################")
+            videoIds = getVideos(topic)
+            print("\n####################################\n" + "VIDEO_IDS USED FOR STATS FOR " + topic + "\n####################################")
+            stats = getStatistics(videoIds)
+            print("\n####################################\n" + "VIDEO_IDS USED FOR COMMENTS FOR " + topic + "\n####################################")
+            #comments = getComments(videoIds)
+            print("\n####################################\n" + "SAVING VIDEO IDS\n" + "####################################")
+
+            with open(csv_output, 'a') as f:
+                for vidId in videoIds.keys():
+                    f.write("\n" +
+                            ",".join([videoIds[vidId][0],
+                                    videoIds[vidId][1],
+                                    videoIds[vidId][2],
+                                    vidId,
+                                    videoIds[vidId][3],
+                                    stats[vidId]['viewCount'],
+                                    stats[vidId]['likeCount'],
+                                    stats[vidId]['favoriteCount'],
+                                    stats[vidId]['commentCount'],
+                                    videoIds[vidId][4]]))
+
+            print("\n####################################\n" + "SAVING COMMENTS\n" + "####################################")
+
+                                    
+
 main()
