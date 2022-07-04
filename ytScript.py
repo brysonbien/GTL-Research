1443347b49f1a51bf3dd4b5a92759057cd59efc6
Max Sikora <msikora8@gatech.edu>
2022-07-04 17:24:22 +0200

    VideoIds Push to A Headered CSV File

--- ytScript.py
+++ ytScript.py
@@ -6,52 +6,80 @@ from googleapiclient.discovery import build
 #Api Key generated 
 api_key = 'AIzaSyDIuQ0gDsXrKNvGIdKMWUezUjkD9Kgrx8s'
 
+csv_output = 'query_responses.csv'
+
 #Building Youtube object
 youtube = build('youtube', 'v3', developerKey=api_key)
 
 #List of keywords
-keyword_list = []
-keyword_list.append("Computer Science Lists")
-# keyword_list.append("Computer Science Stacks")
-# keyword_list.append("Computer Science Queues")
-# keyword_list.append("Computer Science Linked Lists")
-# keyword_list.append("Computer Science Trees, AVL & Binary Trees ")
-# keyword_list.append("Computer Science Graphs")
-# keyword_list.append("Computer Science Trees")
-# keyword_list.append("Computer Science Hashing and hash tables")
-# keyword_list.append("Computer Science Dynamic Programming")
-# keyword_list.append("Computer Science Data Structures")
-# keyword_list.append("Computer Science Algorithms")
-
+keyword_list = ["Computer Science Lists",
+                # "Computer Science Stacks",
+                # "Computer Science Queues",
+                # "Computer Science Linked Lists",
+                # "Computer Science Trees, AVL & Binary Trees",
+                # "Computer Science Graphs",
+                # "Computer Science Trees",
+                # "Computer Science Hashing and hash tables",
+                # "Computer Science Dynamic Programming",
+                # "Computer Science Data Structures",
+                # "Computer Science Algorithms"
+                ]
+                
 """
 Gets the videoIds of the imported search parameters, stores them in a csv file.
 It then uses those CSV to get the top 50 comments for each video.
 """
 def csvOutputVideos():
-    print("####################################\n" + "VIDEO_IDS OUPUTTED TO CSV FILE\n" + "####################################")
+    print("####################################\n" + "COLLECTING VIDEO IDS\n" + "####################################")
     #Iterate through the keyword list to search youtube api and write out to query_responses.txt
+    videoIds = {}
     for string in keyword_list:
-        request = youtube.search().list(
-        part="snippet",
-        type='video',
-        q = string,
-        maxResults = 2
-        )
+        request = youtube.search().list(part="snippet",
+                                        type='video',
+                                        q = string,
+                                        maxResults = 2)
         response = request.execute()
 
-        # Reads videoIds from response .json document and writes to a csv file
-        with open('query_responses.csv', 'w') as f:
-            videoIds = []
-            for elem in response['items']:
-                videoIds.append(elem["id"]["videoId"])
-            f.write(','.join(videoIds))
-    print("\n")
+        nextPageToken = response["nextPageToken"]
 
+        for elem in response['items']:
+            videoId = elem["id"]["videoId"]
+            publishedAt = elem["snippet"]["publishedAt"]
+            channelId = elem["snippet"]["channelId"]
+            title = elem["snippet"]["title"] 
+
+            videoObject =  [title, 
+                            publishedAt, 
+                            channelId, 
+                            nextPageToken]
+
+            videoIds[videoId] = videoObject
+    
+    print("####################################\n" + "SAVING VIDEO IDS\n" + "####################################")
+
+    with open(csv_output, 'w+') as f:
+        f.write(",".join([  "topic",
+                            "title",
+                            "publishedAt",
+                            "videoId",
+                            "channelId",
+                            "nextPageToken"]))
+    with open(csv_output, 'a') as f:
+        for vidId in videoIds.keys():
+            f.write("\n" +
+                    ",".join([videoIds[vidId][0],
+                              videoIds[vidId][1],
+                              vidId,
+                              videoIds[vidId][2],
+                              videoIds[vidId][nextPageToken]]))
+                              
 """
 Gets video statistics from video ID
 """
 def getStatistics():
     print("####################################\n" + "VIDEO_IDS USED FOR STATS\n" + "####################################")
+    
+    
     with open('query_responses.csv', 'r') as f:
         data = f.read()
         split_data = data.split(",")
@@ -96,7 +124,6 @@ def csvOutputComments():
 
 def main():
         csvOutputVideos()
-        csvOutputComments()
-        getStatistics()
-
+        # csvOutputComments()
+        # getStatistics()
 main()
